ad36a651c6961c637fbf0f6c20b7a9c3b97c6457
Ryan Tam <ryantam626@gmail.com>
2022-05-01 13:50:27 +0100

    Publish docs

---
+++ _static/js/ga.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+var newScript = document.createElement("script");
+newScript.type = "text/javascript";
+newScript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-3SYVJHTVEF");
+document.documentElement.firstChild.appendChild(newScript);
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-3SYVJHTVEF');
